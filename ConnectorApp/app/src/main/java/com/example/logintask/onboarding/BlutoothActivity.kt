e8bcf14d6076213b0e0ef2384113b7467177653f
suryodaygit <suryodaygit@gmail.com>
2022-07-18 10:54:16 +0530

    initail
    ok change

--- ConnectorApp/app/src/main/java/com/example/logintask/onboarding/BlutoothActivity.kt
+++ ConnectorApp/app/src/main/java/com/example/logintask/onboarding/BlutoothActivity.kt
@@ -14,7 +14,7 @@ import com.example.logintask.lib.base.BaseActivity
 import com.example.logintask.lib.utils.showToast
 import java.io.*
 import java.util.*
-//comment
+//comment   ok
 
 class BlutoothActivity : BaseActivity() {
 
